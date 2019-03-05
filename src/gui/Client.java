e31601df600080c117ac0e2c7f93d21007e5e324
Nikita Kuzmin <nxk825@cs.bham.ac.uk>
2019-03-05 12:33:06 +0000

    Nick's Test

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -14,8 +14,7 @@ import java.io.*;
 
 public class Client implements Runnable {
 	
-	//This is a test comment to see if GIT  works 
-	// harris test3
+	//This is a test comment to see if GIT  works
 	//Nick test3
 	
 	List<String> hand;
