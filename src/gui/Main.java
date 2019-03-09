8fe4a37e0e4e6c7b1f6f5cf5fcc0638f19588c22
George Wellard <gxw879@cs.bham.ac.uk>
2019-03-09 12:39:01 +0000

    Name change

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-		primaryStage.setTitle("Black Jack-off");
+		primaryStage.setTitle("Club21");
 		Scene scene = new Scene(root, WIDTH, HEIGHT);
 		primaryStage.setResizable(true);
 //		primaryStage.setMinHeight(400);
