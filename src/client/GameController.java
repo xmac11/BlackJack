fcc2bb453848ec831f028de876c78ee8f87f83f5
marcwatts <mxw817@student.bham.ac.uk>
2019-03-14 19:39:05 +0000

    Chat boxes now allow ordinary keyboard punctuation!

--- src/client/GameController.java
+++ src/client/GameController.java
@@ -116,7 +116,7 @@ public class GameController implements Initializable {
 
 	public void sendChat() {
 		String incomingText = textField.getText();
-		if (incomingText.matches("[a-zA-Z\\s0-9]*") && incomingText.trim().length() > 0) {
+		if (incomingText.matches("[a-zA-Z\\s0-9\\s.,!@$%£^&?<>()]*") && incomingText.trim().length() > 0) {
 			output.println("gameChatMessage");
 			output.println("gameChatMessage" + username);
 			output.println("gameChatMessage" + incomingText);
--- src/client/LobbyController.java
+++ src/client/LobbyController.java
@@ -264,7 +264,7 @@ public class LobbyController implements Initializable {
 
 	public void sendChat() {
 		String incomingText = chatField.getText();
-		if (incomingText.matches("[a-zA-Z\\s0-9]*") && incomingText.trim().length() > 0) {
+		if (incomingText.matches("[a-zA-Z\\s0-9\\s.,!@$%£^&?<>()]*") && incomingText.trim().length() > 0) {
 			output.println("lobbyChatMessage");
 			output.println("lobbyChatMessage" + username);
 			output.println("lobbyChatMessage" + incomingText);
